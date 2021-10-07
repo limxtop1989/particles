b19e378471bd705322ce66c0cee5d20322754cc5
Maxim <miaoxin.li@versa-ai.com>
2021-10-07 18:29:56 +0800

    wrap shader with java class.

---
+++ app/src/main/java/com/versa/particles/program/ParticleShaderProgram.java
@@ -0,0 +1,54 @@
+package com.versa.particles.program;
+
+import static android.opengl.GLES20.glGetUniformLocation;
+
+import android.content.Context;
+import android.opengl.GLES20;
+
+import com.versa.particles.R;
+
+public class ParticleShaderProgram extends ShaderProgram {
+
+    // Uniform locations
+    private final int uMatrixLocation;
+    private final int uTimeLocation;
+
+    // Attribute locations
+    private final int aPositionLocation;
+    private final int aColorLocation;
+    private final int aDirectionVectorLocation;
+    private final int aParticleStartTimeLocation;
+
+    protected ParticleShaderProgram(Context context) {
+        super(context, R.raw.particle_vertex_shader, R.raw.particle_fragment_shader);
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
+        uTimeLocation = GLES20.glGetUniformLocation(program, U_TIME);
+
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR);
+        aDirectionVectorLocation = GLES20.glGetAttribLocation(program, A_DIRECTION_VECTOR);
+        aParticleStartTimeLocation = GLES20.glGetAttribLocation(program, A_PARTICLE_START_TIME);
+    }
+
+    public void setUniforms(float[] matrix, float elapsedTime) {
+        // count parameter 1 means one matrix rather than matrix array.
+        GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, matrix, 0);
+        GLES20.glUniform1f(uTimeLocation, elapsedTime);
+    }
+
+    public int getPositionLocation() {
+        return aPositionLocation;
+    }
+
+    public int getColorLocation() {
+        return aColorLocation;
+    }
+
+    public int getDirectionVectorLocation() {
+        return aDirectionVectorLocation;
+    }
+
+    public int getParticleStartTimeLocation() {
+        return aParticleStartTimeLocation;
+    }
+}
